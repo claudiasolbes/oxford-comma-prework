d1b2b18b91135c05a4fe866fa47c290a0dd86d52
Claudia Solbes <cssolbes@hotmail.com>
2019-06-10 03:30:27 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,16 +1,16 @@
 def oxford_comma(array)
-  last = array.pop
+  # last = array.pop
   str = ""
   len = array.length
   if len == 1
-    str << array[0] << " and " << last
+    str << array[0] << " and " << array.pop
   elsif (len > 0 && len != 1)
     array.each do |entry|
       str << entry << ", "
     end
-    str << "and " << last
+    str << "and " << array.pop
   else
-    str << last
+    str << array.pop
   end
   return str
 end
